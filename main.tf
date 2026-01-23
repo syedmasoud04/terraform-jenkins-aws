8f7167ff78d3d9835c7ba90c3dede885d10aa68b
esa <esasyed1001@gmmail.com>
2026-01-23 19:52:11 +0530

    fixed the ec2 instance type

--- main.tf
+++ main.tf
@@ -85,7 +85,7 @@ resource "aws_security_group" "ec2_sg" {
 # Create EC2 instance
 resource "aws_instance" "ec2" {
   ami                    = "ami-0c02fb55956c7d316"
-  instance_type          = "t2.micro"
+  instance_type          = "t3.micro"
   subnet_id              = aws_subnet.public.id
   vpc_security_group_ids = [aws_security_group.ec2_sg.id]
 
